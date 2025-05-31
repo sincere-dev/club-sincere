2d51da2b7553ceb51994ecc66a56e6795ceaea7f
sincere-dev <1sincere.dev@gmail.com>
2025-05-31 17:21:48 -0400

    add phone fallback

--- src/components/PhoneFallback.tsx
+++ src/components/PhoneFallback.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
 
 export const PhoneFallback = () => (
-  <div className="hide-on-desktop">[phone fallback]</div>
+  <div
+    style={{
+      height: '100vh',
+      background: 'black',
+    }}
+    className="hide-on-desktop"
+  >
+    <img
+      style={{ paddingTop: '24px' }}
+      src="/img/head.gif"
+      alt="head"
+    />
+    <p
+      style={{
+        fontFamily: 'Arial',
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        color: 'white',
+        textAlign: 'initial',
+        padding: '40px',
+      }}
+    >
+      the inside of club sincere cannot
+      be viewed using your phone.
+      <br />
+      <br />
+      please use a computer.
+      <br />
+      <br />
+      thank you.
+    </p>
+  </div>
 );
