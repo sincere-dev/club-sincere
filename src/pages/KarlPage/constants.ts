498a7789494be6c42cb9f39c9f61c77dc708d270
sincere-dev <1sincere.dev@gmail.com>
2025-06-01 22:59:42 -0400

    update Karl eyes

--- src/pages/KarlPage/constants.ts
+++ src/pages/KarlPage/constants.ts
@@ -1,11 +1,11 @@
 import { Pill } from './types';
 
-export const PILL_SIZE = 30;
+export const PILL_SIZE = 60;
 export const KARL_SIZE = 180;
 export const KARL = 'karl';
 
 export const initialPills: Pill[] = [
-  { id: 'pill-1', top: 20, left: 20, consumed: false },
+  { id: 'pill-1', top: 20, left: 30, consumed: false },
   // { id: 'pill-2', top: 20, left: window.innerWidth - PILL_SIZE - 20, consumed: false },
   // { id: 'pill-3', top: window.innerHeight - PILL_SIZE - 20, left: 20, consumed: false },
 ];
